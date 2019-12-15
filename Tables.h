bb03adad3188637be564d92ba1d43fa5198b3f9e
Trần Ngọc Huy <truongduchuy910@gmail.com>
2019-12-16 04:42:11 +0700

    add

---
+++ Tables.h
@@ -0,0 +1,28 @@
+#ifndef TABLES_H
+#define TABLES_H
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+class Menu;
+class Menu
+{
+public:
+	int foodId;
+	string name;
+	double cost;
+	Menu()
+	{
+		this->foodId = 0;
+		this->cost = 0.0;
+	}
+	static void head()
+	{
+		cout << setw(6) << "foodId" << setw(6) << "name" << setw(6) << "cost" << endl;
+	}
+	const bool operator==(const Menu&);
+	friend ostream& operator<<(ostream&, const Menu&);
+	friend istream& operator>>(istream&, Menu&);
+};
+#endif
