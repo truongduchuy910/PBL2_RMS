0b47bada3d7088a6d15106f592128b5468a550b1
Nguyên Vũ Hoàng <hoangnguyenvubk@gmail.com>
2019-12-16 11:48:15 +0700

    them controllers vaf views

---
+++ Controllers.h
@@ -0,0 +1,21 @@
+class Controllers;
+class Controllers
+{
+public:
+    void home();
+    void menu();
+    void menuShow();
+    void menuAdd();
+    void menuRemove();
+    void deskShow();
+    void deskAdd();
+    void deskRemove();
+    void desk();
+    void order();
+    void orderAdd();
+    void bill();
+    void billShow();
+    void billAdd();
+    //void billOrder();
+   // void billPayment();
+};
