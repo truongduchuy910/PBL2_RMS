c0acfe38e714130f57a66a06a9fa8f71f7e5c2e6
Nguyên Vũ Hoàng <hoangnguyenvubk@gmail.com>
2019-12-16 12:14:20 +0700

    them views vaf controllers

---
+++ RMS/Views.h
@@ -0,0 +1,19 @@
+class Views;
+class Views
+{
+public:
+    int home();
+    int menu();
+    int menuShow();
+    Menu menuAdd();
+    Menu menuRemove();
+    int desk();
+    Desk deskAdd();
+    Desk deskRemove();
+    int deskShow();
+    int order();
+    Order orderAdd();
+    int bill();
+    Bill billAdd();
+    //int billShow();
+};
